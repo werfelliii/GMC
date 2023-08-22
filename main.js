961c1530ca386f66b780cf940c34d7537ce8ca47
werfelliii <youssefouerfelli0@gmail.com>
2023-08-22 09:21:32 +0100

    second

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 //*theme mode*//
 var element = document.getElementsByClassName("theme")[0];
-
+const root = document.documentElement;
 element.addEventListener("click", function(event) {
-  if(element.innerHTML=="Dark mode"){
-    root.style.setProperty('--ecriture', 'rgba(0, 0, 0, 0.76)');
-    root.style.setProperty('--background', '#f1f1f1');
-    element.innerHTML="Light Mode"}
+  if(element.innerHTML=="Light Mode"){
+    root.style.setProperty('--ecriture', 'rgba(121, 10, 10, 0.76)');
+    root.style.setProperty('--background', '#03cdff');
+    element.innerHTML="Dark mode"}
     else{
-        root.style.setProperty('--background', 'rgba(0, 0, 0, 0.76)');
-        root.style.setProperty('--ecriture', '#f1f1f1');
-        element.innerHTML="Dark mode";}
+        root.style.setProperty('--background', 'rgba(121, 10, 10, 0.76)');
+        root.style.setProperty('--ecriture', '#03cdff');
+        element.innerHTML="Light Mode";}
     
 });
 
@@ -18,6 +18,5 @@ element.addEventListener("click", function(event) {
 
 
 
-const root = document.documentElement;
 
-// Change the values of the CSS variables
+
