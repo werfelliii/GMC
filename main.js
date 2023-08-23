b79a14329e9da7a81e16b38ea6e44c4fa007d16c
werfelliii <youssefouerfelli0@gmail.com>
2023-08-23 10:04:47 +0100

    fifa

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ var element = document.getElementsByClassName("theme")[0];
 const root = document.documentElement;
 element.addEventListener("click", function(event) {
   if(element.innerHTML=="Light Mode"){
-    root.style.setProperty('--ecriture', 'rgba(121, 10, 10, 0.76)');
-    root.style.setProperty('--background', '#03cdff');
+    root.style.setProperty('--ecriture', '#262626');
+    root.style.setProperty('--background', '#f1f1f1');
     element.innerHTML="Dark mode"}
     else{
-        root.style.setProperty('--background', 'rgba(121, 10, 10, 0.76)');
-        root.style.setProperty('--ecriture', '#03cdff');
+        root.style.setProperty('--background', '#262626');
+        root.style.setProperty('--ecriture', '#f1f1f1');
         element.innerHTML="Light Mode";}
     
 });
@@ -17,7 +17,7 @@ element.addEventListener("click", function(event) {
 
 const modalContainer = document.querySelector("#c1");
 console.log(modalContainer,"helo")
-const modalTriggers = document.querySelectorAll(".modal-trigger");
+const modalTriggers = document.querySelectorAll(".bc");
 console.log(modalContainer.style,"hello")
 function toggleModal(event) {
   console.log("Toggle modal function executed.");
@@ -31,3 +31,21 @@ function toggleModal(event) {
 modalTriggers.forEach(trigger => {
   trigger.addEventListener("click", toggleModal);
 });
+//fifa//
+
+const modalContainers = document.querySelector("#c2");
+const modalTriggers1 = document.querySelectorAll(".bf");
+console.log(modalContainers.style.display,"helbo")
+function toggleModal1(event) {
+  console.log("Toggle modal function executed.");
+  if (modalContainers.style.display == "") {
+    modalContainers.style.display = "flex";
+  } else {
+    modalContainers.style.display = "";
+  }
+}
+
+modalTriggers1.forEach(trigger => {
+  trigger.addEventListener("click", toggleModal1);
+});
+
